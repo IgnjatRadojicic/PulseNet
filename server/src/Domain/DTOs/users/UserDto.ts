070dd4605948f3dbcace4e924843b7e6c9e03079
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-03-29 21:27:24 +0200

    feat: added neccessary auth dtos

--- server/src/Domain/DTOs/users/UserDto.ts
+++ server/src/Domain/DTOs/users/UserDto.ts
@@ -1,7 +1,12 @@
 export class UserDto {
   public constructor(
-    public id: number = 0,
-    public korisnickoIme: string = "",
-    public uloga: string = "user",
+        public id: number = 0,
+        public username: string = '',
+        public email: string = '',
+        public name: string = '',
+        public lastname: string = '',
+        public bio: string | null = null,
+        public profilePicture: string | null = null, 
+        public role: string = 'user',
   ) {}
 }
--- server/src/Domain/models/User.ts
+++ server/src/Domain/models/User.ts
@@ -1,8 +1,13 @@
 export class User {
   public constructor(
-    public id: number = 0,
-    public korisnickoIme: string = '',
-    public uloga: string = 'user',
-    public lozinka: string = ''
+        public id: number = 0,
+        public username: string = '',
+        public email: string = '',
+        public name: string = '',
+        public lastname: string = '',
+        public bio: string | null = null,
+        public profilePicture: string | null = null, 
+        public role: string = 'user',
+        public password: string = ''
   ) {}
 }
