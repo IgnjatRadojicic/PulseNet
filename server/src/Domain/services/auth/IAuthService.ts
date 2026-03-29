b6ff005dba59efc12dd3dd83c088a144cd5fad16
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-03-30 00:31:37 +0200

    feat: Add IAuthService interface

--- server/src/Domain/services/auth/IAuthService.ts
+++ server/src/Domain/services/auth/IAuthService.ts
@@ -1,20 +1,15 @@
-import { UserAuthDataDto } from "../../DTOs/auth/UserAuthDataDto";
+import { UserAuthDataDto } from '../../DTOs/auth/UserAuthDataDto';
+import { ServiceResult } from '../../types/ServiceResult';
 
 export interface IAuthService {
-    /**
-     * Prijavljuje korisnika sa datim korisničkim imenom i lozinkom.
-     * @param korisnickoIme - Korisničko ime korisnika.
-     * @param lozinka - Lozinka korisnika.
-     * @returns Podatke o korisniku ako je prijava uspešna, ili prazan objekat ako nije.
-     */
-  prijava(korisnickoIme: string, lozinka: string): Promise<UserAuthDataDto>;
-
-  /**
-   * Registruje novog korisnika sa datim korisničkim imenom i lozinkom.
-   * @param korisnickoIme - Korisničko ime korisnika.
-   * @param lozinka - Lozinka korisnika.
-   * @param uloga - Uloga korisnika u sistemu.
-   * @returns Podatke o korisniku ako je registracija uspešna, ili prazan objekat ako nije.
-  */
-  registracija(korisnickoIme: string, uloga: string, lozinka: string): Promise<UserAuthDataDto>;
+    login(username: string, password: string): Promise<ServiceResult<UserAuthDataDto>>;
+    register(
+        username: string,
+        email: string,
+        firstName: string,
+        lastName: string,
+        password: string,
+        bio?: string,
+        profileImage?: string
+    ): Promise<ServiceResult<UserAuthDataDto>>;
 }
