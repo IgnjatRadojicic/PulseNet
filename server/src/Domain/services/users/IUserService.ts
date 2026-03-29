03e6c4d3d71f93c49c029af445108db7a8e82ea8
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-03-30 00:31:37 +0200

    feat: Add IUserService interface

--- server/src/Domain/services/users/IUserService.ts
+++ server/src/Domain/services/users/IUserService.ts
@@ -1,9 +1,22 @@
-import { UserDto } from "../../DTOs/users/UserDto";
+import { UserDto } from '../../DTOs/users/UserDto';
+import { ServiceResult } from '../../types/ServiceResult';
 
 export interface IUserService {
-     /**
-     * Vraca listu svih korisnika u sistemu.
-     * @returns Podatke o korisnicima u vidu liste.
-     */
-  getSviKorisnici(): Promise<UserDto[]>;
+    getAllUsers(): Promise<ServiceResult<UserDto[]>>;
+    getUserById(id: number): Promise<ServiceResult<UserDto>>;
+    updateProfile(
+        id: number,
+        username: string,
+        email: string,
+        firstName: string,
+        lastName: string,
+        bio?: string,
+        profileImage?: string
+    ): Promise<ServiceResult<UserDto>>;
+    updateRole(id: number, role: string): Promise<ServiceResult<boolean>>;
+    searchUsers(query: string): Promise<ServiceResult<UserDto[]>>;
+    followUser(followerId: number, followingId: number): Promise<ServiceResult<boolean>>;
+    unfollowUser(followerId: number, followingId: number): Promise<ServiceResult<boolean>>;
+    getFollowers(id: number): Promise<ServiceResult<UserDto[]>>;
+    getFollowing(id: number): Promise<ServiceResult<UserDto[]>>;
 }
