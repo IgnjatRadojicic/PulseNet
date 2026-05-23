e5ec17148beff9539299cbf789f1ca8894cf5162
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-05-23 21:47:33 +0200

    feat: add getCommentCountBatch method to IPostCommentRepository interface

--- server/src/Domain/repositories/post_repository/IPostCommentRepository.ts
+++ server/src/Domain/repositories/post_repository/IPostCommentRepository.ts
@@ -2,4 +2,5 @@ import { Post } from '../../models/Post';
 
 export interface IPostCommentRepository {
     getCommentCount(postId: number): Promise<number>;
+    getCommentCountBatch(postIds: number[]): Promise<Map<number, number>>;    
 }
