eda8b7d7b1cb57953bdd867582d15be66621eab8
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-05-23 23:43:07 +0200

    refactor: update IAuthApiService to use ApiResponse types with import type

--- client/src/api_services/auth/IAuthAPIService.ts
+++ client/src/api_services/auth/IAuthAPIService.ts
@@ -1,18 +1,7 @@
-import type { AuthResponse } from '../../types/auth/AuthResponse';
+import type { ApiResponse } from '../../helpers/api';
 
-
-export interface IAuthAPIService {
-  login(username: string, password: string): Promise<AuthResponse>
-  register(
-        username: string,
-        email: string,
-        firstName: string,
-        lastName: string,
-        password: string,
-        bio?: string,
-        profileImage?: string
-    ): Promise<AuthResponse>;
-    
-    logout(token: string): Promise<void>;
-   
+export interface IAuthApiService {
+    login(username: string, password: string): Promise<ApiResponse<string>>;
+    register(username: string, email: string, firstName: string, lastName: string, password: string, bio?: string, profileImage?: string): Promise<ApiResponse<string>>;
+    logout(): Promise<ApiResponse<void>>;
 }
